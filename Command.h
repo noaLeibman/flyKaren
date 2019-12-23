5966c2c26095a49c061e245af5f324894d9f4e67
Hani <haninus106@gmail.com>
2019-12-23 16:07:34 +0200

    Add existing file

---
+++ Command.h
@@ -0,0 +1,12 @@
+//
+// Created by hani on 23/12/2019.
+//
+
+#ifndef EX3_COMMAND_H
+#define EX3_COMMAND_H
+
+#endif //EX3_COMMAND_H
+class Command{
+public:
+    virtual int execute();
+};
---
+++ OpenServerCommand.h
@@ -0,0 +1,8 @@
+//
+// Created by hani on 23/12/2019.
+//
+
+#ifndef EX3_OPENSERVERCOMMAND_H
+#define EX3_OPENSERVERCOMMAND_H
+
+#endif //EX3_OPENSERVERCOMMAND_H
